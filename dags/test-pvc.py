ccf4f2603401e0a9fa6c456f0886e10ff6119ce0
Madhav Prajapati <madhav.prajapati@solvei8.com>
2025-03-26 14:28:49 +0530

    pvc added

--- dags/test-pvc.py
+++ dags/test-pvc.py
@@ -5,6 +5,7 @@ import os
 
 DAG_ID = "python_operator_pvc_example"
 SHARED_VOLUME_MOUNT_PATH = "/shared"
+PVC_NAME = "airflow-shared-pvc"
 FILE_PATH = os.path.join(SHARED_VOLUME_MOUNT_PATH, "my_file.txt")
 
 def create_file():
@@ -28,12 +29,26 @@ with DAG(
         task_id="create_file",
         python_callable=create_file,
         op_kwargs={},
+        volumes=[{
+            'name': 'shared-volume',
+            'persistentVolumeClaim': {
+                'claimName': PVC_NAME
+            }
+        }],
+        volume_mounts=[{'name': 'shared-volume', 'mountPath': SHARED_VOLUME_MOUNT_PATH}]
     )
 
     read_file_task = PythonOperator(
         task_id="read_file",
         python_callable=read_file,
         op_kwargs={},
+        volumes=[{
+            'name': 'shared-volume',
+            'persistentVolumeClaim': {
+                'claimName': PVC_NAME
+            }
+        }],
+        volume_mounts=[{'name': 'shared-volume', 'mountPath': SHARED_VOLUME_MOUNT_PATH}]
     )
 
     create_file_task >> read_file_task
