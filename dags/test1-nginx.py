6f226a895a5b544aebdcd0cbda3a90bac085b869
Madhav Prajapati <madhav.prajapati@solvei8.com>
2025-03-26 13:35:00 +0530

    change the name nginx

---
+++ dags/test1-nginx.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+
+# Define DAG arguments
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 3, 24),
+    "catchup": False
+}
+
+# Define the DAG
+dag = DAG(
+    "launch_nginx_pod-2",
+    default_args=default_args,
+    schedule_interval=None
+)
+
+# KubernetesPodOperator to launch an NGINX pod
+nginx_pod = KubernetesPodOperator(
+    task_id="nginx",
+    name="nginx-pod",
+    namespace="airflow",  # Change this to match your Airflow namespace
+    image="nginx:latest",
+    cmds=["nginx", "-g", "daemon off;"],  # Keeps the container running
+    get_logs=True,
+    dag=dag
+)
+
+# Task execution
+nginx_pod
+
