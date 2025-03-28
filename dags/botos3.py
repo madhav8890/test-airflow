34d4ad675f337c9a9a7b197d239b832fe24ad97d
Madhav Prajapati <madhav.prajapati@solvei8.com>
2025-03-28 14:22:11 +0530

    ect lsit

--- dags/botos3.py
+++ dags/botos3.py
@@ -1,36 +1,43 @@
+import json
 from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
-from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
-from airflow.utils.dates import days_ago
+from airflow.providers.amazon.aws.hooks.ec2 import EC2Hook
+from airflow.operators.python import PythonOperator
+from datetime import datetime
 
-# Fetch AWS credentials from Airflow connection
-aws_hook = AwsBaseHook(aws_conn_id='my_aws', client_type='sts')
-credentials = aws_hook.get_credentials()
+# Define default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+}
 
 # Define the DAG
-dag = DAG(
-    'list_ec2_instances_k8s',
-    schedule_interval='@daily',
-    start_date=days_ago(1),
+with DAG(
+    dag_id='list_ec2_instances',
+    default_args=default_args,
+    description='List all EC2 instances',
+    schedule_interval=None,  # Change to a cron expression if needed
+    start_date=datetime(2025, 3, 28),
     catchup=False,
-)
+) as dag:
 
-# KubernetesPodOperator to run AWS CLI
-list_instances_task = KubernetesPodOperator(
-    task_id='list_instances',
-    name='aws-cli-list-instances',
-    namespace='default',  # Change namespace if needed
-    image='amazon/aws-cli',  # AWS CLI official image
-    cmds=["sh", "-c"],
-    arguments=[
-        "aws ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,State.Name]' --output table"
-    ],
-    env_vars={
-        'AWS_ACCESS_KEY_ID': credentials.access_key,
-        'AWS_SECRET_ACCESS_KEY': credentials.secret_key,
-        'AWS_DEFAULT_REGION': 'us-east-1',  # Change as needed
-    },
-    dag=dag,
-)
+    def list_instances(**kwargs):
+        # Create an EC2 hook using the connection ID
+        ec2_hook = EC2Hook(aws_conn_id='my_aws')
+        
+        # Get all instance IDs
+        instance_ids = ec2_hook.get_instance_ids()
+        
+        # Print out the instance IDs and their states
+        print("EC2 Instance IDs and their states:")
+        for instance_id in instance_ids:
+            instance_info = ec2_hook.get_instance(instance_id)
+            print(f"Instance ID: {instance_info.id}, State: {instance_info.state['Name']}")
 
-list_instances_task
+    # Define a PythonOperator to execute the function
+    list_ec2_instances_task = PythonOperator(
+        task_id='list_ec2_instances_task',
+        python_callable=list_instances,
+        provide_context=True,
+    )
+
+list_ec2_instances_task
