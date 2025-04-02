7a805f82f62422c2eb62c7f80a2614dc2d7e11f4
Madhav Prajapati <madhav.prajapati@solvei8.com>
2025-04-02 14:23:13 +0530

    test-ssh connection

--- dags/ssh_connection.py
+++ dags/ssh_connection.py
@@ -15,7 +15,8 @@ dag = DAG(
 ssh_task = SSHOperator(
     task_id="test_ssh_connection",
     ssh_conn_id="ssh_remote",  # Airflow Connection ID
-    command="sudo bash /home/ubuntu/get_info.sh",
+    command="sudo bash {{ params.script_path }}",
+    params={"script_path": "/home/ubuntu/get_info.sh"},
     dag=dag,
 )
 
