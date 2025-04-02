8a9eed757fed6e3c50f44ad64978fdd590fd95f7
Madhav Prajapati <madhav.prajapati@solvei8.com>
2025-04-02 13:10:09 +0530

    test-ssh connection

---
+++ dags/ssh_connection.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.utils.dates import days_ago
+
+# Define DAG
+dag = DAG(
+    "ssh_test_dag",
+    default_args={"owner": "airflow"},
+    schedule_interval=None,  # Run manually
+    start_date=days_ago(1),
+    catchup=False,
+)
+
+# SSH Task: Runs "date" on the remote server
+ssh_task = SSHOperator(
+    task_id="test_ssh_connection",
+    ssh_conn_id="ssh_remote",  # Airflow Connection ID
+    command="date",
+    dag=dag,
+)
+
+ssh_task  # Task execution
