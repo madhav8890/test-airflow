8da3a058bf1f4cc0c9e85f1f58020bb15b77dbc1
Madhav Prajapati <madhav.prajapati@solvei8.com>
2025-04-02 14:41:05 +0530

    test-ssh connection

--- dags/ssh_connection.py
+++ dags/ssh_connection.py
@@ -1,23 +1,56 @@
+# from airflow import DAG
+# from airflow.providers.ssh.operators.ssh import SSHOperator
+# from airflow.utils.dates import days_ago
+
+# # Define DAG
+# dag = DAG(
+#     "ssh_test_dag",
+#     default_args={"owner": "airflow"},
+#     schedule_interval=None,  # Run manually
+#     start_date=days_ago(1),
+#     catchup=False,
+# )
+
+# # SSH Task: Runs "date" on the remote server
+# ssh_task = SSHOperator(
+#     task_id="test_ssh_connection",
+#     ssh_conn_id="ssh_remote",  # Airflow Connection ID
+#     command="sudo bash {{ params.script_path }}",
+#     params={"script_path": "/home/ubuntu/get_info.sh"},
+#     dag=dag,
+# )
+
+# ssh_task  # Task execution
+
 from airflow import DAG
 from airflow.providers.ssh.operators.ssh import SSHOperator
 from airflow.utils.dates import days_ago
+from airflow.decorators import task
+
+# List of Hosts
+host_list = ["10.0.139.192", "10.0.136.28", "10.0.149.221"]  # Add more hosts as needed
 
 # Define DAG
 dag = DAG(
-    "ssh_test_dag",
+    "ssh_parallel_test_dag",
     default_args={"owner": "airflow"},
     schedule_interval=None,  # Run manually
     start_date=days_ago(1),
     catchup=False,
 )
 
-# SSH Task: Runs "date" on the remote server
-ssh_task = SSHOperator(
-    task_id="test_ssh_connection",
-    ssh_conn_id="ssh_remote",  # Airflow Connection ID
-    command="sudo bash {{ params.script_path }}",
-    params={"script_path": "/home/ubuntu/get_info.sh"},
-    dag=dag,
-)
+# Function to create SSH tasks dynamically
+@task
+def generate_ssh_tasks(host):
+    return SSHOperator(
+        task_id=f"ssh_task_{host.replace('.', '_')}",
+        ssh_conn_id="ssh_remote",  # Airflow Connection ID (Make sure it works for all hosts)
+        command="sudo bash {{ params.script_path }}",
+        params={"script_path": "/home/ubuntu/get_info.sh"},
+        dag=dag,
+        do_xcom_push=False,  # Disable XCom for efficiency
+    )
 
-ssh_task  # Task execution
+# Expand tasks dynamically for each host
+ssh_tasks = generate_ssh_tasks.expand(host=host_list)
+
